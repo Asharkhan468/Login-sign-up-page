9012bfe3d8d69a6e2a6cf1ca1d22735119217cb5
Ashar Khan <asharullah.khan2468@gmail.com>
2024-06-27 16:40:13 +0500

    login page

---
+++ app.js
@@ -0,0 +1,14 @@
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
+const email = document.querySelector("#Email");
+const password = document.querySelector("#password");
+
+const button = document.querySelector("#button");
+
+const form = document.querySelector("form");
+
+
+
+
+
+
