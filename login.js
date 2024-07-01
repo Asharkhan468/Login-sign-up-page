82f3b408844d5b84799f7eac4402dadf860b4ea6
Ashar Khan <asharullah.khan2468@gmail.com>
2024-07-01 18:19:47 +0500

    Login page quiz app

--- login.js
+++ login.js
@@ -4,6 +4,7 @@ console.log(storeData[0]);
 const loginEmail = document.getElementById("email");
 const loginPassword = document.getElementById("password");
 const btn = document.getElementById("btn-submit");
+const div =  document.getElementById('div');
 
 btn.addEventListener("click", (event) => {
   event.preventDefault();
@@ -12,11 +13,13 @@ btn.addEventListener("click", (event) => {
     loginEmail.value === storeData[0].userEmail &&
     loginPassword.value === storeData[0].password
   ) {
-    console.log("Malik kam sahi horaha hy bilkul");
     window.location = "quiz app.html";
   } else if (loginEmail.value === "" && loginPassword.value === "") {
-    console.log("Malik apny data enter nahi kiya");
-  } else {
-    console.log("Malik kuch tu garbar hy mailk!");
+    alert('Please fill all the field');
+  } else if(password.value==''|| password.value != storeData[0].password) {
+    alert('Please Enter the password Again')
+
+  }else{
+    alert('Error')
   }
 });
