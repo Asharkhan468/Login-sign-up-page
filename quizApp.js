c0ac5b8af2e59a662f26d186e08d2f63bb58a9ce
Ashar Khan <asharullah.khan2468@gmail.com>
2024-07-04 11:17:26 +0500

    Login signup done

--- quizApp.js
+++ quizApp.js
@@ -1,4 +1,10 @@
 const quiz = document.querySelector("#quiz");
+const option1 = document.querySelector("#option1");
+const option2 = document.querySelector("#option2");
+const option3 = document.querySelector("#option3");
+const option4 = document.querySelector("#option4");
+const btnSubmit = document.querySelector("#btn-submit"); 
+const option = document.getElementsByName("option");
 axios(
   "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple"
 )
@@ -8,23 +14,24 @@ axios(
     resp.data.results.map((item)=>{
         let answer = [item.correct_answer , ...item.incorrect_answers];
         quiz.innerHTML += `<h4>${item.question}</h4>
-        <div class="list-none">
+        <form class="list-none">
     <li>
-        <input type="radio" id="option1" value="" > <label for="option1">${answer[0]}</label>
+        <input type="radio" id="option1" value="${answer[0]}" > <label name="option">${answer[0]}</label>
     </li>
 
      <li>
-        <input type="radio" value="" > <label for="option1">${answer[1]}</label>
+        <input type="radio" id="option2" value="${answer[1]}" > <label name="option">${answer[1]}</label>
     </li>
 
      <li>
-        <input type="radio" value="" > <label for="option1">${answer[2]}</label>
+        <input type="radio" id="option3" value="${answer[2]}" > <label name="option">${answer[2]}</label>
     </li>
 
      <li>
-        <input type="radio" value="" > <label for="option1">${answer[3]}</label>
+        <input type="radio" id="option4" value="${answer[3]}" > <label name="option">${answer[3]}</label>
     </li>
-  </div><hr/>`;
+  </form><hr/>`;
+
 
     })
 
@@ -33,3 +40,11 @@ axios(
     console.log(err);
 
 })
+
+btnSubmit.addEventListener('click' , (event)=>{
+    
+})
+
+
+
+
