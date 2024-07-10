b2f4b177868eb18ca13fea0145cd2d7365955a8b
Ashar Khan <asharullah.khan2468@gmail.com>
2024-07-10 17:45:02 +0500

    Quiz app

--- quizApp.js
+++ quizApp.js
@@ -1,50 +1,122 @@
-const quiz = document.querySelector("#quiz");
-const option1 = document.querySelector("#option1");
-const option2 = document.querySelector("#option2");
-const option3 = document.querySelector("#option3");
-const option4 = document.querySelector("#option4");
-const btnSubmit = document.querySelector("#btn-submit"); 
-const option = document.getElementsByName("option");
+
+//Navbar started
+
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdownToggle = document.getElementById("dropdownMenuButton");
+  const dropdownMenu = document.getElementById("dropdownMenu");
+
+  dropdownToggle.addEventListener("click", () => {
+    dropdownMenu.style.display =
+      dropdownMenu.style.display === "block" ? "none" : "block";
+  });
+
+  window.addEventListener("click", (e) => {
+    if (!dropdownToggle.contains(e.target)) {
+      dropdownMenu.style.display = "none";
+    }
+  });
+});
+
+
+// localStorage.getItem('')
+
+
+
+
+
+
+
+//Navbar ended
+
+
+
+
+
+
+
+
+let div = document.querySelector("#div");
+let ul = document.querySelector("#div-ul");
+let index = 0;
+let question = [];
+
+
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+
+let renderData = (arr)=>{
+
+  let answer = [...arr[index].incorrect_answers, arr[index].correct_answer];
+
+  div.innerHTML = `<h4>Q${index + 1}: ${arr[index].question}</h4>`;
+
+  shuffleArray(answer).map((item)=>{
+     ul.innerHTML += `<li class="list-none">
+        <input type="radio" name="choice" class="choice" id=${item} value=${item}><label for=${item}>${item}</label>
+        </li>`;
+
+  })
+
+}
+
+
+
+
+
+
+
+
+
 axios(
   "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple"
 )
-
-.then((resp)=>{
+  .then((resp) => {
     console.log(resp.data.results);
-    resp.data.results.map((item)=>{
-        let answer = [item.correct_answer , ...item.incorrect_answers];
-        quiz.innerHTML += `<h4>${item.question}</h4>
-        <form class="list-none">
-    <li>
-        <input type="radio" id="option1" value="${answer[0]}" > <label name="option">${answer[0]}</label>
-    </li>
+    question = resp.data.results;
+    renderData(question)
+  })
 
-     <li>
-        <input type="radio" id="option2" value="${answer[1]}" > <label name="option">${answer[1]}</label>
-    </li>
-
-     <li>
-        <input type="radio" id="option3" value="${answer[2]}" > <label name="option">${answer[2]}</label>
-    </li>
-
-     <li>
-        <input type="radio" id="option4" value="${answer[3]}" > <label name="option">${answer[3]}</label>
-    </li>
-  </form><hr/>`;
-
-
-    })
-
-})
-.catch((err)=>{
+  .catch((err) => {
     console.log(err);
-
-})
-
-btnSubmit.addEventListener('click' , (event)=>{
-    
-})
+  });
 
 
+
+
+
+
+  //footer started
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const homeButton = document.getElementById("homeButton");
+    const categoriesButton = document.getElementById("categoriesButton");
+    const profileButton = document.getElementById("profileButton");
+
+    homeButton.addEventListener("click", () => {
+      // Logic to navigate to home
+      alert("Home button clicked");
+    });
+
+    categoriesButton.addEventListener("click", () => {
+      // Logic to navigate to categories
+      alert("Categories button clicked");
+    });
+
+    profileButton.addEventListener("click", () => {
+      // Logic to navigate to profile
+      alert("Profile button clicked");
+    });
+  });
+
+
+
+  //footer ended
 
 
